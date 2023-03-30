21bacfe73910dedf4213e7b7b52bf7f69cadff5a
Ausra <munduswebstudio@gmail.com>
2023-03-30 09:44:19 +0300

    Console.log()

---
+++ index.js
@@ -0,0 +1,17 @@
+// Vienos eilutes komentaras
+
+/*
+Multi
+line
+komentaras
+*/
+
+console.log(42); // Komentaras salia
+console.log(41);
+console.log(40);
+
+// extra
+
+console.log(42);
+console.log(41);
+console.log(40);
