1a665e219c5d876cd4b15c023f36879cae876608
Ausra <munduswebstudio@gmail.com>
2023-04-06 13:46:18 +0300

    Intro into FOR loop

--- js/09-for.js
+++ js/09-for.js
@@ -12,17 +12,22 @@ const count = marks.length;
 // =
 
 let sum = 0;
-sum = sum + marks[0];
-console.log('Suma:', sum);
-sum = sum + marks[1];
-console.log('Suma:', sum);
-sum = sum + marks[2];
-console.log('Suma:', sum);
-sum = sum + marks[3];
-console.log('Suma:', sum);
-sum = sum + marks[4];
+let index = 0;
+
+sum += marks[index++];
 console.log('Suma:', sum);
 
+sum += marks[index++];
+console.log('Suma:', sum);
+
+sum += marks[index++];
+console.log('Suma:', sum);
+
+sum += marks[index++];
+console.log('Suma:', sum);
+
+sum += marks[index++];
+console.log('Suma:', sum);
 
 const average = sum / count;
 console.log('Vidurkis:', average);
