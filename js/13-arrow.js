287a8d0a0b76eded98d383df02f23f375afe292d
Ausra <munduswebstudio@gmail.com>
2023-04-11 14:33:14 +0300

    Arrow

---
+++ js/13-arrow.js
@@ -0,0 +1,97 @@
+console.clear();
+
+/*
+ARROW function - rodyklines funkcijos
+
+Skirtingi budai uzrasyti funcija.
+*/
+
+
+const pirmas = 7;
+const antras = 5;
+
+// function declaration . tipinis variantas, kaip galima uzrasyti funkcija.
+
+function sum (a, b) {
+    return a + b;
+}
+console.log(`${pirmas} + ${antras} = ${sum(pirmas, antras)}`);
+
+function diff (a, b) {
+    return a - b;
+}
+console.log(`${pirmas} - ${antras} = ${diff(pirmas, antras)}`);
+
+function div (a, b) {
+    return a / b;
+}
+console.log(`${pirmas} / ${antras} = ${div(pirmas, antras)}`);
+
+function multi (a, b) {
+    return a * b;
+}
+console.log(`${pirmas} * ${antras} = ${multi(pirmas, antras)}`);
+
+function square (a) {
+    return a * a;
+}
+console.log(`${pirmas} ** 2 = ${square(pirmas)}`);
+
+console.log('---------------------------');
+
+// anonimous function
+const diff2 = function (a, b) {
+    return a - b;
+}
+console.log(`${pirmas} - ${antras} = ${diff2(pirmas, antras)}`);
+
+const sum2 = function (a, b) {
+    return a + b;
+}
+console.log(`${pirmas} + ${antras} = ${sum2(pirmas, antras)}`);
+
+const div2 = function (a, b) {
+    return a / b;
+}
+console.log(`${pirmas} / ${antras} = ${div2(pirmas, antras)}`);
+
+const multi2 = function (a, b) {
+    return a * b;
+}
+console.log(`${pirmas} * ${antras} = ${multi2(pirmas, antras)}`);
+
+const square2 = function (a) {
+    return a * a;
+}
+console.log(`${pirmas} ** 2 = ${square2(pirmas)}`);
+
+console.log('---------------------------');
+
+// arrow function (rodykline funkcija)
+const div3 = (a, b) => {
+    return a / b;
+}
+console.log(`${pirmas} / ${antras} = ${div3(pirmas, antras)}`);
+
+const multi3 = (a, b) => {
+    return a * b;
+}
+console.log(`${pirmas} * ${antras} = ${multi3(pirmas, antras)}`);
+
+console.log('---------------------------');
+
+// jeigu logikos bloke yra TIK 1 salyga (statement)
+// galime nerasyti: {return}
+
+const multi4 = (a, b) => a * b;
+console.log(`${pirmas} * ${antras} = ${multi4(pirmas, antras)}`);
+
+const square3 = (a) => a * a;
+console.log(`${pirmas} ** 2 = ${square3(pirmas)}`);
+
+console.log('---------------------------');
+
+// jeigu parametru bloke yra TIK 1 parametras
+// galima nerasyti: ()
+const square4 = a => a * a;
+console.log(`${pirmas} ** 2 = ${square4(pirmas)}`);
